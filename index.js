dea4f90cc89c03e19bb0a15b02996dfa93866659
Luoana Chirita <luoana.livadariu@gmail.com>
2018-08-25 11:32:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,5 +14,7 @@ function scuberGreetingForFeet(someValue) {
 }
 
 
-function ternaryCheckCity(destination) {destination == "NYC" ? message = 'Ok, sounds good.' : message = 'No go'
-return message}
+function ternaryCheckCity(destination) {destination == "NYC" ? message = 'Ok, sounds good.' : message = 'No go.'
+return message
+  
+}
