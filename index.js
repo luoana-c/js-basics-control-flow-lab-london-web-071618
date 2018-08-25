20e3d5bdab45e7c160b921ac2b9d370dc69a3c1c
Luoana Chirita <luoana.livadariu@gmail.com>
2018-08-25 11:24:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 function scuberGreetingForFeet(someValue) {
   if (someValue <= 400) {
     message = 'This one is on me!'
-    
   }
   else if (someValue > 2000) {
     message = 'I will gladly take your thirty bucks.'
