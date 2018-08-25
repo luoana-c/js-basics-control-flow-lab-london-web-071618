8f2dedaa910dcf1c4edeaaa03f526a07f04cc130
Luoana Chirita <luoana.livadariu@gmail.com>
2018-08-25 11:38:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your code in this file!
+function scuberGreetingForFeet(someValue) {
+  if (someValue <= 400) {
+    message = 'This one is on me!'
+  }
+  else if (someValue > 2000 && someValue < 2500) {
+    message = 'I will gladly take your thirty bucks.'
+  }
+  else if (someValue > 2500) {
+    message = 'No can do.'
+  }
+  
+  return message
+}
+
+
+function ternaryCheckCity(destination) {
+  destination == "NYC" ? message = 'Ok, sounds good.' : message = 'No go.'
+  return message
+}
+
+
+
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous': 
+      response = "Thank you so much.";
+      break;
+    case 'not as generous': 
+      response = "Thank you.";
+      break;
+    default:
+      response = "Bye."
+      break;
+  }
+  return response;
+}
